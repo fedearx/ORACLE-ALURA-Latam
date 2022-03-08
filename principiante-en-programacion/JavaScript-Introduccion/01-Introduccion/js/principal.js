7c784afdc80d72c8e527fa0e6f608ed29029832a
Federico A. Rubio <fedearx@gmail.com>
2022-03-08 11:49:48 -0300

    01. Introsucción

---
+++ principiante-en-programacion/JavaScript-Introduccion/01-Introduccion/js/principal.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector(".titulo");
+console.log(titulo);
+console.log(titulo.textContent);
+titulo.textContent = "Buena vida Nutrición";
+console.log("Estoy imprimiendo de una fuente externa que es principal.js");
