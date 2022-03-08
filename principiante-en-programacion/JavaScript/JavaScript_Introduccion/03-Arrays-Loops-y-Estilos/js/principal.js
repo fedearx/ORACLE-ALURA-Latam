29d45b2cb17deeb8f217b895216ca3520a5228d6
Federico A. Rubio <fedearx@gmail.com>
2022-03-08 15:35:14 -0300

    03. Arrays, Loop y Estilos

---
+++ principiante-en-programacion/JavaScript/JavaScript_Introduccion/03-Arrays-Loops-y-Estilos/js/principal.js
@@ -0,0 +1,42 @@
+// querySelector --> solo selecciona un solo elemento
+// var pacientes = document.querySelector(".paciente");
+
+var pacientes = document.querySelectorAll(".paciente");    // id #    Clase .
+
+for (var i = 0; i < pacientes.length; i++) {
+
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdIMC = paciente.querySelector(".info-imc");
+
+  var pesoEsValido = true;
+  var alturaEsValida = true;
+
+  if ((peso < 0) || (peso > 1000)) {
+    tdIMC.textContent = "Peso Incorrecto";
+    pesoEsValido = false;
+    // paciente.style.color = "lightcoral";               pinta todas las letras de la fila en rojo
+    // paciente.style.backgroundColor = "lightcoral";     pinta todas las celdas de la fila en rojo
+    paciente.classList.add("paciente-incorrecto");
+  }
+
+  if ((altura < 0) || (altura > 3)) {
+    tdIMC.textContent = "Altura Incorrecta";
+    alturaEsValida = false;
+    paciente.classList.add("paciente-incorrecto");
+  }
+
+  if (pesoEsValido && alturaEsValida) {
+    var imc = peso / (altura * altura)   // calculó el IMC
+    tdIMC.textContent = imc.toFixed(2);   // toFixed(2), que tenga dos decimales
+  }
+
+}
+
+
